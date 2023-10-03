ea4e1f1a6a7bca4e23eecda6d82860c11cfccae0
Ibrahima <iab.jr10@gmail.com>
2023-10-03 06:00:45 -0400

    Update App.css, App.js, index.js, Content.js, NavTabs.js, and About.js Change background color in css Import React and Content.js in App.js Comment out some code in index.js Import React and files from pages folder to Content.js, add function to render page and handle page change Add navbar to NavTabs.js Add some filler text to About.js

---
+++ my-react-app/src/components/Content.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+
+
+
+function Content() {
+    const [currentPage, setCurrentPage] = useState('About');
+
+
+    const renderPage = () => {
+        switch(currentPage) {
+            case 'Contact':
+                return <Contact />;
+                // break;
+            case 'Portfolio':
+                return <Portfolio />;
+                // break;
+            case 'Resume':
+                return <Resume />;
+                // break;
+            default:
+                return <About />
+        }
+    };
+
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+            {renderPage()}
+        </div>
+    );
+}
+
+
+export default Content;
---
+++ my-react-app/src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>Here's a bunch of text about me</p>
+            <p>This is where I'm from</p>
+            <p>Here's a picture of me</p>
+            <p>This is what I like to do</p>
+            <p>This is what I like to read</p>
+            <p>This is what I like to watch</p>
+            <img src=''></img>
+
+        </div>
+    )
+}
