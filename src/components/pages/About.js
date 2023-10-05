20796a280870e57449d9c68e0f2f7a0be25d689a
Ibrahima <iab.jr10@gmail.com>
2023-10-05 05:20:20 -0400

    Move logo.svg, update css file, add content to About.js, and add image to assets folder

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import ib from '../assets/ib.jpg';
 
 export default function About() {
     return (
         <div>
             <h1>About Me</h1>
             <p>Here's a bunch of text about me</p>
-            <p>This is where I'm from</p>
+            <p>    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis quasi voluptatem inventore labore odit fugit tempora illum. Consequatur maiores excepturi sequi, nobis magnam soluta deleniti amet eius. Repellendus, blanditiis odit?</p>
             <p>Here's a picture of me</p>
+            <img src={ib} alt='Ibrahima' id='ib'></img>
             <p>This is what I like to do</p>
             <p>This is what I like to read</p>
             <p>This is what I like to watch</p>
-            <img src=''></img>
+
 
         </div>
     )
