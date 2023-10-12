48b605727a95da507e4fe3235740476c81b21a16
Ibrahima <iab.jr10@gmail.com>
2023-10-12 00:30:52 -0400

    Add media query for large viewing and change picture for readme project

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -12,51 +12,57 @@ export default function Portfolio() {
         <section className='portfolio'>
             <h1>Portfolio</h1>
             <p>Here are the projects I have completed</p>
-            <div className='project'>
-            <a href='https://iab-19.github.io/odd_quote_out_kanye_edition/'><p>Odd Quote Out: Kanye Edition</p></a>
-                <img src={kanye} alt='Odd Quote Out' className='portfolio-image'></img>
-                <div>
-                    <a href='https://github.com/iab-19/odd_quote_out_kanye_edition'><img src={githubIcon} className='github-icon' alt="github link"></img></a>
+            <div className='row'>
+                <div className='project'>
+                <a href='https://iab-19.github.io/odd_quote_out_kanye_edition/'><p>Odd Quote Out: Kanye Edition</p></a>
+                    <img src={kanye} alt='Odd Quote Out' className='portfolio-image'></img>
+                    <div>
+                        <a href='https://github.com/iab-19/odd_quote_out_kanye_edition'><img src={githubIcon} className='github-icon' alt="github link"></img></a>
+                    </div>
+                </div>
+
+                <div className='project'>
+                    <p>SVG Logo Generator</p>
+                    <img src={svg} alt='SVG Generator' className='portfolio-image'></img>
+                    <div>
+                        <a href='https://github.com/iab-19/svg-logo-maker'><img src={githubIcon} className='github-icon' alt="github link"></img></a>
+                    </div>
                 </div>
             </div>
 
-            <div className='project'>
-                <p>SVG Logo Generator</p>
-                <img src={svg} alt='SVG Generator' className='portfolio-image'></img>
-                <div>
-                    <a href='https://github.com/iab-19/svg-logo-maker'><img src={githubIcon} className='github-icon' alt="github link"></img></a>
+            <div className='row'>
+                <div className='project'>
+                    <p>Professional README Generator</p>
+                    <img src={readme} alt='readme generator' className='portfolio-image'></img>
+                    <div>
+                        <a href='https://github.com/iab-19/professional-readme-generator'><img src={githubIcon} className='github-icon' alt="github link"></img></a>
+                    </div>
+                </div>
+
+                <div className='project'>
+                    <p>Employee Tracker</p>
+                    <img src={employee} alt='Employee Tracker' className='portfolio-image'></img>
+                    <div>
+                        <a href='https://github.com/iab-19/employee-tracker'><img src={githubIcon} className='github-icon' alt="github link"></img></a>
+                    </div>
                 </div>
             </div>
 
-            <div className='project'>
-                <p>Professional README Generator</p>
-                <img src={readme} alt='readme generator' className='portfolio-image'></img>
-                <div>
-                    <a href='https://github.com/iab-19/professional-readme-generator'><img src={githubIcon} className='github-icon' alt="github link"></img></a>
+            <div className='row'>
+                <div className='project'>
+                <a href='https://iab-19.github.io/weather_dashboard/'><p>Weather Dashboard</p></a>
+                    <img src={weather} alt='Weather Dashboard' className='portfolio-image'></img>
+                    <div>
+                        <a href='https://github.com/iab-19/weather_dashboard'><img src={githubIcon} className='github-icon' alt="github link"></img></a>
+                    </div>
                 </div>
-            </div>
 
-            <div className='project'>
-                <p>Employee Tracker</p>
-                <img src={employee} alt='Employee Tracker' className='portfolio-image'></img>
-                <div>
-                    <a href='https://github.com/iab-19/employee-tracker'><img src={githubIcon} className='github-icon' alt="github link"></img></a>
-                </div>
-            </div>
-
-            <div className='project'>
-            <a href='https://iab-19.github.io/weather_dashboard/'><p>Weather Dashboard</p></a>
-                <img src={weather} alt='Weather Dashboard' className='portfolio-image'></img>
-                <div>
-                    <a href='https://github.com/iab-19/weather_dashboard'><img src={githubIcon} className='github-icon' alt="github link"></img></a>
-                </div>
-            </div>
-
-            <div className='project'>
-            <a href='https://iab-19.github.io/work-day-scheduler/'><p>Workday Scheduler</p></a>
-                <img src={scheduler} alt='Workday Scheduler' className='portfolio-image'></img>
-                <div>
-                    <a href='https://github.com/iab-19/work-day-scheduler'><img src={githubIcon} className='github-icon' alt="github link"></img></a>
+                <div className='project'>
+                <a href='https://iab-19.github.io/work-day-scheduler/'><p>Workday Scheduler</p></a>
+                    <img src={scheduler} alt='Workday Scheduler' className='portfolio-image'></img>
+                    <div>
+                        <a href='https://github.com/iab-19/work-day-scheduler'><img src={githubIcon} className='github-icon' alt="github link"></img></a>
+                    </div>
                 </div>
             </div>
 
