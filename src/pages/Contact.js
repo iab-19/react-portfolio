6292b93138864a14429e67ac4447f041fa0730bf
Ibrahima <iab.jr10@gmail.com>
2023-11-21 19:00:40 -0500

    Add code to send email in contact section

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,13 +3,13 @@ export default function Contact() {
     return (
         <section className='contact'>
             <h1>Contact</h1>
-            <form id='contactForm'>
+            <form target='_blank' action='https://formsubmit.co/fb748a76f150d7c07604372283283bf0' method='POST' id='contactForm'>
                 <label >Name:</label><br />
-                <input type="text" id='name'></input><br />
+                <input type="text" id='name' name='name' required></input><br />
                 <label >Email Address:</label><br />
-                <input type='email'></input><br />
+                <input type='email' name='email' required></input><br />
                 <label >Message</label><br />
-                <textarea type='text' id="message" rows="5" cols="40"></textarea><br /> <br />
+                <textarea type='text' id="message" rows="5" cols="40" name='message' required></textarea><br /> <br />
                 <input type='submit' value="Submit" id='submitButton'></input>
             </form>
         </section>
